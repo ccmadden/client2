00898d2ffbb72d0ddd3fb6861cf8f4baf6735169
Cate Madden <158115037+ccmadden@users.noreply.github.com>
2024-09-19 13:43:53 -0400

    Add files via upload

--- client2.py
+++ client2.py
@@ -1,11 +1,11 @@
 import csv
 import os
 
-folder_path = 'meet' #not sure if this is correctly getting to the folder
+directory = 'meet' #not sure if this is correctly getting to the folder
 output_file = "LamplighterInvite23.html"
 
 # Loop through files in the directory
-for file_name in os.listdir(folder_path):
+for file_name in os.listdir(directory):
     file_path = os.path.join(folder_path, file_name)
     if os.path.isfile(file_path):
         # Open or process the file
